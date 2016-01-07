2c489239db67789c3583c88924cf31e15fa21b8a
wangbj <wangbj@gmail.com>
2016-01-07 12:18:06 -0800

    Finished Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,17 +16,14 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) h (Compose f) = Compose $ ( (<$>) . (<$>) ) h f
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure f = Compose $ (pure . pure) f
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) (Compose f) (Compose g) = Compose $ lift2 (<*>) f g
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
